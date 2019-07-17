a30e3f53c247c44a5cc44d6a0b02b76fd1d61e40
xybh <1353433900@qq.com>
2019-07-17 23:11:40 +0800

    完成github登录

--- src/main/java/online/lanwang/community/controller/AuthorizeController.java
+++ src/main/java/online/lanwang/community/controller/AuthorizeController.java
@@ -1,4 +1,39 @@
 package online.lanwang.community.controller;
 
+import online.lanwang.community.dto.AccessTokenDTO;
+import online.lanwang.community.dto.GitHubUser;
+import online.lanwang.community.provider.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GitHubProvider gitHubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_url(redirectUrl);
+        String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
+        GitHubUser gitHubUser = gitHubProvider.gitUser(accessToken);
+        System.out.println(gitHubUser.getName());
+        return "index";
+    }
 }
--- src/main/java/online/lanwang/community/provider/GitHubProvider.java
+++ src/main/java/online/lanwang/community/provider/GitHubProvider.java
@@ -1,4 +1,55 @@
 package online.lanwang.community.provider;
 
+import com.alibaba.fastjson.JSON;
+import okhttp3.*;
+import online.lanwang.community.dto.AccessTokenDTO;
+import online.lanwang.community.dto.GitHubUser;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
 public class GitHubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType
+                = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            try {
+                String string = response.body().string();
+                String token = string.split("&")[0].split("=")[1];
+                return token;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GitHubUser gitUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            GitHubUser gitHubUser = JSON.parseObject(string, GitHubUser.class);
+            return gitHubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
