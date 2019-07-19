e8f7eeb6b2d23fda30c5d392c8991e24e3f8a151
xybh <1353433900@qq.com>
2019-07-19 12:25:05 +0800

    增加数据库功能

--- src/main/java/online/lanwang/community/controller/AuthorizeController.java
+++ src/main/java/online/lanwang/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package online.lanwang.community.controller;
 
 import online.lanwang.community.dto.AccessTokenDTO;
 import online.lanwang.community.dto.GitHubUser;
+import online.lanwang.community.mapper.UserMapper;
+import online.lanwang.community.model.User;
 import online.lanwang.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,6 +27,9 @@ public class AuthorizeController {
     @Value("${github.redirect.url}")
     private String redirectUrl;
 
+    @Autowired
+    UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
@@ -37,8 +43,15 @@ public class AuthorizeController {
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GitHubUser gitHubUser = gitHubProvider.gitUser(accessToken);
         if (gitHubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(gitHubUser.getName());
+            user.setAccountId(String.valueOf(gitHubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             //登录成功,写cookie和session
-            request.getSession().setAttribute("user",gitHubUser);
+            request.getSession().setAttribute("user", gitHubUser);
             return "redirect:/";        //重定向至首页,地址栏清空
         } else {
             //登录失败,重新登录
--- src/main/java/online/lanwang/community/mapper/UserMapper.java
+++ src/main/java/online/lanwang/community/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package online.lanwang.community.mapper;
 
-public class UserMapper {
+import online.lanwang.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Created by xybh on 2019/7/19
+ */
+@Mapper
+public interface UserMapper{
+    @Insert("Insert into user (name,account_id,token,gmt_Create,gmt_Modified) value (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
 }
